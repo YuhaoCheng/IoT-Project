44e09b2bc41cd037ff9ab1162ffed3e9f2c3703e
YuhaoCheng <446358161@qq.com>
2018-03-31 12:13:14 +0800

    update the main method

--- pycom/gateway/main.py
+++ pycom/gateway/main.py
@@ -30,18 +30,49 @@ import socket
 import util
 from network import LoRa
 
+def handle_save(buf, file_name):
+    print("The method to save the data")
+
+
+
 if __name__=='__main__':
     print('This is the gateway node!')
-    lora = LoRa(mode=LoRa.LORA)
+    # lora = LoRa(mode=LoRa.LORA, rx_iq=True)
     COUNT = 0 # To record the number of the message
+    buf = {} # To record the data temporally
+    sn = 1 # To count the file
     request = '{"GatewayID":"1","Messagetype":"request"}' # the request to the server
     result = util.sendRequest()
 
+    lora = LoRa(mode=LoRa.LORA, rx_iq=True)
+    lora_sock = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+    lora_sock.setblocking(False)
+
     result_dict = json.dumps(result)
-    time = result_dict['DateTime'] # Get the time from the server
+    date_time = result_dict['DateTime'] # Get the time from the server
     device_list = result_dict['DeviceList'] # Get the device list from the server
 
-    
+    # node_handler = threading.Thread(target=handle_lora_reveive(), args=(device_list, date_time,))
+    # node_handler.start()
+
+    while True:
+        COUNT = COUNT + 1
+        recv_pkg = lora_sock.recv(1024)
+
+
+        if COUNT==9:
+            file_name = str(date_time,sn)
+            sn = sn + 1
+            save_handler = threading.Thread(target=handle_save(), args=(buf, file_name,))
+            save_handler.start()
+
+
+
+
+
+
+
+
 
 
 
